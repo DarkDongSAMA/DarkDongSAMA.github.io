81d31fd2792978359d91ade5c647f8979706764b
動 <51774400+DarkDongSAMA@users.noreply.github.com>
2025-01-07 14:12:13 +0800

    Add files via upload

--- kumoko/static/components/baito.vue
+++ kumoko/static/components/baito.vue
@@ -18,71 +18,6 @@
       </div>
       <button style="margin-left: 20px" @click="sleep">睡觉</button>
     </div>
-    <div v-if="user.isStop">
-      <div v-if="place == -1">
-        <div>选择出行地点</div>
-        <div>
-          <button
-            v-for="(item, index) in placeList"
-            :key="index"
-            @click="changePlace(index)"
-            :disabled="user.date < item.lock"
-          >
-            {{ item.name }}
-          </button>
-        </div>
-      </div>
-      <div v-else>
-        <button @click="changePlace(-1)">返回</button>
-        <div>选择行动</div>
-        <div style="display: flex">
-          <div v-for="(item, index) in actList" :key="index" @click="changeMenu(index)">
-            <button>{{ item }}</button>
-          </div>
-        </div>
-        <div v-if="menu == 0">
-          <div>休息菜单</div>
-          <div>
-            <div
-              v-for="(item, index) in placeList[place].children[menu]"
-              :key="index"
-              @click="changeMenu(index)"
-            >
-              <button @click="doAction(item)">{{ item.name }}</button>
-              <span>{{ item.content }}</span>
-            </div>
-          </div>
-        </div>
-        <div v-if="menu == 1">
-          <div>打工菜单</div>
-          <div>
-            <div
-              v-for="(item, index) in placeList[place].children[menu]"
-              :key="index"
-              @click="changeMenu(index)"
-            >
-              <button @click="doAction(item)">{{ item.name }}</button>
-              <span>{{ item.content }}</span>
-            </div>
-          </div>
-        </div>
-        <div v-if="menu == 2">
-          <div>探索菜单</div>
-          <div>
-            <div
-              v-for="(item, index) in placeList[place].children[menu]"
-              :key="index"
-              @click="changeMenu(index)"
-            >
-              <button @click="doAction(item)">{{ item.name }}</button>
-              <span>{{ item.content }}</span>
-            </div>
-          </div>
-        </div>
-        <div v-if="menu == 3">钓鱼菜单</div>
-        <div v-if="menu == 4">挖矿菜单</div>
-      </div>
-    </div>
     <div style="margin-top: 20px">
       自动行动说明：<br />
       休息：Lv.{{ user.actLv[0] }} (恢复 {{ 15 + user.actLv[0] * 5 }} 体力)<br />
@@ -106,8 +41,6 @@ module.exports = {
   data() {
     return {
       active: 0,
-      menu: 0,
-      place: -1,
     };
   },
   methods: {
@@ -115,21 +48,9 @@ module.exports = {
     sleep() {
       goToTomorrow(this);
     },
-    // 操作
-    doAction(item) {
-      item.fun(this);
-    },
-    // 切换菜单
-    changeMenu(index) {
-      this.menu = index;
-    },
     // 选择行动
     changeIndex(index) {
       this.active = index;
-    },
-    // 选择出行地点
-    changePlace(index) {
-      this.place = index;
     },
     // 更新行动
     updateAct(index) {
---
+++ kumoko/static/components/travel.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div>外出</div>
+    <div v-if="place == -1">
+      <div>选择出行地点</div>
+      <div>
+        <button
+          v-for="(item, index) in placeList"
+          :key="index"
+          @click="changePlace(index)"
+          :disabled="user.date < item.lock"
+        >
+          {{ item.name }}
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <button @click="changePlace(-1)">返回</button>
+      <div>选择行动</div>
+      <div style="display: flex">
+        <div v-for="(item, index) in actList" :key="index" @click="changeMenu(index)">
+          <button>{{ item }}</button>
+        </div>
+      </div>
+      <div v-if="tMenu == 0">
+        <div>休息菜单</div>
+        <div>
+          <div
+            v-for="(item, index) in placeList[place].children[tMenu]"
+            :key="index"
+            @click="changeMenu(index)"
+          >
+            <button @click="doAction(item)">{{ item.name }}</button>
+            <span>{{ item.content }}</span>
+          </div>
+        </div>
+      </div>
+      <div v-if="tMenu == 1">
+        <div>打工菜单</div>
+        <div>
+          <div
+            v-for="(item, index) in placeList[place].children[tMenu]"
+            :key="index"
+            @click="changeMenu(index)"
+          >
+            <button @click="doAction(item)">{{ item.name }}</button>
+            <span>{{ item.content }}</span>
+          </div>
+        </div>
+      </div>
+      <div v-if="tMenu == 2">
+        <div>探索菜单</div>
+        <div>
+          <div
+            v-for="(item, index) in placeList[place].children[tMenu]"
+            :key="index"
+            @click="changeMenu(index)"
+          >
+            <button @click="doAction(item)">{{ item.name }}</button>
+            <span>{{ item.content }}</span>
+          </div>
+        </div>
+      </div>
+      <div v-if="tMenu == 3">钓鱼菜单</div>
+      <div v-if="tMenu == 4">挖矿菜单</div>
+    </div>
+  </div>
+</template>
+<script>
+module.exports = {
+  name: "travelPage",
+  props: {
+    user: Object,
+  },
+  data() {
+    return {
+      tMenu: 0,
+      place: -1,
+    };
+  },
+  methods: {
+    // 操作
+    doAction(item) {
+      item.fun(this);
+    },
+    // 切换菜单
+    changeMenu(index) {
+      this.tMenu = index;
+    },
+    // 选择出行地点
+    changePlace(index) {
+      this.place = index;
+    },
+  },
+};
+</script>
